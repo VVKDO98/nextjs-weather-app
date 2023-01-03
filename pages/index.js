604a235e12ccefaff762aa6d63e78d263e0dff7e
VVKDO98 <vanvolcksom.doriano01@gmail.com>
2023-01-03 09:46:31 +0100

    add env variable

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,13 @@ import useSWR from 'swr'
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
 export default function Home() {
-  const {isLoading, error, data} = useSWR('https://api.openweathermap.org/data/2.5/weather?q=charleroi&appid=f013d08632f75fab5f3d3211add116f0', fetcher)
+  // const {isLoading, error, data} = useSWR(`https://api.openweathermap.org/data/2.5/weather?q=charleroi&appid=${process.env.API_KEY}`, fetcher)
   
-  if (error) return <div>Failed to load</div>
-  if (isLoading) return <div>Loading ..</div>
+  // if (error) return <div>Failed to load</div>
+  // if (isLoading) return <div>Loading ..</div>
 
-  console.log(data);
+  // console.log(data);
+  console.log(process.env.API_KEY);
   return (
     <>
       <main>
